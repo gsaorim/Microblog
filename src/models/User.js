693c068ea492e8dbaef14bfdd9ec1c7fe97721e2
gsaorim <148399103+gsaorim@users.noreply.github.com>
2025-10-05 13:17:47 -0300

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -6,29 +6,24 @@ import { Model } from './Model.js';
                    & monta   SQL       criado
                    SQL */
 
-export class User extends Model   //extend = herdar
+export class User extends Model   
 {
     constructor(database)
     {
-        //When create an User, call the first constructor of the class Model(super)
         super(database);
 
-        //Overwrite the name of the table to 'users
         this.tableName = 'users';
     }
 
     async findByEmail(email)
     {
         try {
-            const sql = `SELECT * FROM users WHERE email = $1`;  //first parameter
-            //Execute query passing email as a parameter
+            const sql = `SELECT * FROM users WHERE email = $1`;  
             const result = await this.db.query(sql, [email]);
             
-            //If it didn't find any users return null
             if (result.rows.length === 0) {
                 return null;
             }
-            //if it did, return the first user
             return result.rows[0];
         } catch (error) {
             this.logger.error('Erro ao buscar usuário por email:', error);
@@ -39,7 +34,6 @@ export class User extends Model   //extend = herdar
     async searchByName(name) 
     {
         try {
-            //Select users where username has a parameter (ignoring upper and low cases)
             const sql = `SELECT * FROM users WHERE username ILIKE $1`;
             const result = await this.db.query(sql, [`%${name}%`]);
             
@@ -53,18 +47,15 @@ export class User extends Model   //extend = herdar
     // Validation before create an user
     async create(userData) 
     {
-        // Basic Validations
-        //If miss an email or username, throw an error
         if (!userData.email || !userData.username) {
             throw new Error('Email e username são obrigatórios');
         }
 
-        // Verify if the email exist
         const existingUser = await this.findByEmail(userData.email);
         if (existingUser) {
             throw new Error('Email já está em uso');
         }
-        //If it pass all the validations, call method create original of Model class
         return super.create(userData);
     }
+
 }
